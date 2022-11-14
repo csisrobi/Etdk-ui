857c5763640acaeb31da19bcf759e1662dd5dbf6
csisrobi <csisrobi@gmail.com>
2022-11-14 22:38:12 +0200

    created participation condition page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import MainPage from "../components/MainPage";
+import ParticipationCondition from "../components/ParticipationCondition";
 import WhyApplicate from "../components/WhyApplicate";
 
 const Home: NextPage = () => {
   return (
     <>
-      {/* TODO: SEO */}
+      {/* TODO: SEO FROM STRAPI*/}
       <Head>
         <title>ETDK</title>
-        <meta name="description" content="Etdk" />
+        <meta
+          name="description"
+          content="Erdélyi Tudományos diákköri konferencia reál és humántudományok"
+        />
         <link rel="icon" href="/ETDK.png" />
       </Head>
       <MainPage />
       <WhyApplicate />
+      <ParticipationCondition />
     </>
   );
 };
