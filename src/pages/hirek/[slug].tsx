283554fd39f18664f5f5e4987f3e4b915be41dbc
csisrobi <csisrobi@gmail.com>
2022-12-09 20:32:43 +0200

    build fix

--- src/pages/hirek/[slug].tsx
+++ src/pages/hirek/[slug].tsx
@@ -1,26 +1,26 @@
 import { newsDescription } from "@lib/queries";
 import { getClient } from "@lib/sanity";
 import RichText from "@utils/RichText";
+import type { GetServerSideProps } from "next";
 import type { SanityRichText } from "types";
 
 const Hirek = ({ description }: { description: SanityRichText[] }) => {
   return <RichText blocks={description} />;
 };
 
-export async function getServerSideProps({
+export const getServerSideProps: GetServerSideProps = async ({
   preview = false,
   params,
-}: {
-  preview: boolean;
-  params: unknown;
-}) {
-  const newsData = await getClient(preview).fetch(newsDescription(params.slug));
+}) => {
+  const newsData = await getClient(preview).fetch(
+    newsDescription(params?.slug as string)
+  );
   return {
     props: {
       description: newsData[0].description,
       preview,
     },
   };
-}
+};
 
 export default Hirek;
