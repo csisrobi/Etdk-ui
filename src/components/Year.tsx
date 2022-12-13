8d5789a7a1739674ad91fbff0162707fe9d5918a
csisrobi <csisrobi@gmail.com>
2022-12-14 01:50:11 +0200

    fixed link issue

--- src/components/Year.tsx
+++ src/components/Year.tsx
@@ -13,24 +13,24 @@ const Year = () => {
   return (
     <div
       id="year"
-      className="relative -z-20 flex min-h-[70vh] flex-col items-center gap-10 bg-lightGray py-10 md:justify-center md:pt-0 lg:bg-white"
+      className="relative flex min-h-[70vh] flex-col items-center gap-10 bg-lightGray py-10 md:justify-center md:pt-0 lg:bg-white"
     >
       <div>
         <span className="text-7xl text-darkcherry">2023</span>
       </div>
-      <div className="flex flex-col flex-wrap items-center justify-evenly gap-8 sm:flex-row md:w-3/4">
+      <div className="relative z-10 flex flex-col flex-wrap items-center justify-evenly gap-8 sm:flex-row md:w-3/4">
         {yearElements.map((element) => (
           <LinkWrapper key={element.text} href={element.link || "#"}>
-            <div className="w-80 rounded-3xl bg-beige p-2 text-center text-3xl tracking-wide text-darkcherry">
+            <div className="w-80 cursor-pointer rounded-3xl bg-beige p-2 text-center text-3xl tracking-wide text-darkcherry">
               <span>{element.text}</span>
             </div>
           </LinkWrapper>
         ))}
       </div>
-      <div className="absolute right-0 top-10 -z-10 hidden h-[300px] w-[400px] md:block">
+      <div className="absolute right-0 top-10 hidden h-[300px] w-[400px] md:block">
         <Image src="/zoldhullamjobb.png" alt="zoldhullam" fill />
       </div>
-      <div className="absolute left-0 bottom-10 -z-10 hidden h-[300px] w-[400px] md:block">
+      <div className="absolute left-0 bottom-10 hidden h-[300px] w-[400px] md:block">
         <Image src="/zoldhullambal.png" alt="zoldhullam" fill />
       </div>
     </div>
