316183cdf3926e241b815d2f1ef5b3145aef7981
csisrobi <csisrobi@gmail.com>
2023-01-21 00:02:11 +0200

    font fix again

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com/" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans&display=swap"
           rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans&display=swap"
           as="style"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans&display=swap"
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans&display=swap"
         />
       </Head>
       <body>
