70dbc37e76d708d98b1c0a65d2df7cf502fcafe2
csisrobi <csisrobi@gmail.com>
2023-03-22 18:17:09 +0200

    added checking

--- src/pages/letoltheto-dokumentumok/index.tsx
+++ src/pages/letoltheto-dokumentumok/index.tsx
@@ -14,51 +14,50 @@ const LetolthetoDokumentumok = ({ files }: Props) => {
   return (
     <div className="flex min-h-[100vh] min-w-full flex-col bg-lightGray px-6 pb-6 pt-[100px] text-black  md:px-10 md:pb-10 lg:bg-lightcherry ">
       <div className="flex flex-col items-center justify-center space-y-4 p-6 lg:bg-lightGray">
-        <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
-          <a target="_blank" href={files.certificateURL} rel="noreferrer">
-            Témavezetői igazolás
-          </a>
-        </div>
-        <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
-          <a target="_blank" href={files.contributionURL} rel="noreferrer">
-            Adatbankos nyilatkozat - a kivonatos füzetre vonatkozóan
-          </a>
-        </div>
-        <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
-          <a
-            target="_blank"
-            href={files.declarationProjectURL}
-            rel="noreferrer"
-          >
-            Adatbankos nyilatkozat - a dolgozatra vonatkozóan
-          </a>
-        </div>
-        <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
-          <a target="_blank" href={files.declarationURL} rel="noreferrer">
-            Hozzájárulási nyilatkozat - a Biológia szekcióban résztvevőknek
-          </a>
-        </div>
+        {files.certificateURL && (
+          <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
+            <a target="_blank" href={files.certificateURL} rel="noreferrer">
+              Témavezetői igazolás
+            </a>
+          </div>
+        )}
+        {files.contributionURL && (
+          <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
+            <a target="_blank" href={files.contributionURL} rel="noreferrer">
+              Adatbankos nyilatkozat - a kivonatos füzetre vonatkozóan
+            </a>
+          </div>
+        )}
+        {files.declarationProjectURL && (
+          <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
+            <a
+              target="_blank"
+              href={files.declarationProjectURL}
+              rel="noreferrer"
+            >
+              Adatbankos nyilatkozat - a dolgozatra vonatkozóan
+            </a>
+          </div>
+        )}
+        {files.declarationURL && (
+          <div className="w-fit rounded-lg bg-lightcherry px-4 py-2 text-white">
+            <a target="_blank" href={files.declarationURL} rel="noreferrer">
+              Hozzájárulási nyilatkozat - a Biológia szekcióban résztvevőknek
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export async function getStaticProps({ preview = false }) {
+export async function getServerSideProps({ preview = false }) {
   const files = await getClient(preview).fetch(queryFiles);
-  if (!files.length) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
   return {
     props: {
       files: files[0],
       preview,
     },
-    revalidate: 30,
   };
 }
 
